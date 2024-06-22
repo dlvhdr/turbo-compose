52694cf20e3eea471031f45593003df22ac1cf90
Dolev Hadar <dolevc2@gmail.com>
2024-06-22 23:06:32 +0300

    refactor: tidy up

--- cmd/root.go
+++ cmd/root.go
@@ -1,74 +1,26 @@
-/*
-Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"os"
 	"strings"
 
 	"github.com/docker/docker/api/types/image"
-	"github.com/docker/docker/client"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v3"
 )
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use: "turbo-compose",
 	Run: func(cmd *cobra.Command, args []string) {
-		apiClient, err := client.NewClientWithOpts(client.FromEnv)
+		images, err := GetLocalImages()
 		if err != nil {
 			panic(err)
 		}
-		defer apiClient.Close()
-		images, err := apiClient.ImageList(context.Background(), image.ListOptions{})
-		if err != nil {
-			panic(err)
-		}
-		imgDict := make(map[string]image.Summary)
-		for _, img := range images {
-			firstTag := ""
-			tags := img.RepoTags
-			if len(tags) > 0 {
-				firstTag = tags[0]
-			}
-			nameParts := strings.Split(firstTag, ":")
-			name := "none"
-			if len(nameParts) > 1 {
-				name = strings.Join(nameParts[:len(nameParts)-1], ":")
-			}
-			imgDict[name] = img
-		}
-		services := listFromComposeFile()
-		for _, service := range services {
-			fmt.Printf("%v\n", service.Image)
-		}
-		opts := make([]ServiceOption, 0)
-		for _, service := range services {
-			if !strings.HasPrefix(service.Image, "634375685434.dkr.ecr.us-east-1.amazonaws.com") {
-				continue
-			}
-			// name := strings.Split(service.Image, ":")[0]
-			// shortName := strings.Split(name, "/")[1]
-			name := service.Name
-			if img, ok := imgDict[name]; ok {
-				opts = append(opts, ServiceOption{
-					Name:       name,
-					Image:      service.Image,
-					LocalImage: &img,
-				})
-			} else {
-				opts = append(opts, ServiceOption{
-					Name:       name,
-					Image:      service.Image,
-					LocalImage: nil,
-				})
-			}
-		}
+		services := listServicesFromComposeFile()
+
+		opts := makeOpts(services, images)
 
 		selection := make([]ServiceOption, 0)
 		form := GetForm(opts, &selection)
@@ -89,19 +41,16 @@ func Execute() {
 	}
 }
 
-func listFromComposeFile() []Service {
+func listServicesFromComposeFile() []Service {
 	yamlFile, err := os.ReadFile("/Users/dlvhdr/code/komodor/mono/docker-compose.yml")
 	if err != nil {
 		log.Fatalf("Error reading YAML file: %v", err)
 	}
-	// Parse the YAML content into a DockerCompose struct
 	var dockerCompose DockerCompose
 	err = yaml.Unmarshal(yamlFile, &dockerCompose)
 	if err != nil {
 		log.Fatalf("Error unmarshaling YAML: %v", err)
 	}
-	// Print the parsed DockerCompose struct
-	// fmt.Printf("Version: %s\n", dockerCompose.Version)
 	res := make([]Service, 0)
 	for serviceName, service := range dockerCompose.Services {
 		if strings.HasPrefix(service.Image, "634375685434.dkr.ecr.us-east-1.amazonaws.com") {
@@ -110,6 +59,30 @@ func listFromComposeFile() []Service {
 	}
 
 	return res
+}
+
+func makeOpts(services []Service, images map[string]image.Summary) []ServiceOption {
+	opts := make([]ServiceOption, 0)
+	for _, service := range services {
+		if !strings.HasPrefix(service.Image, "634375685434.dkr.ecr.us-east-1.amazonaws.com") {
+			continue
+		}
+		name := service.Name
+		if img, ok := images[name]; ok {
+			opts = append(opts, ServiceOption{
+				Name:       name,
+				Image:      service.Image,
+				LocalImage: &img,
+			})
+		} else {
+			opts = append(opts, ServiceOption{
+				Name:       name,
+				Image:      service.Image,
+				LocalImage: nil,
+			})
+		}
+	}
+	return opts
 }
 
 type ServiceOption struct {
