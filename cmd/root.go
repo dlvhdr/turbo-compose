8a1cdbb6efa4191cd28b3b0841a615b429b3d546
Dolev Hadar <dolevc2@gmail.com>
2024-06-22 18:52:54 +0300

    Init

---
+++ cmd/root.go
@@ -0,0 +1,29 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "turbo-compose",
+	Short: "",
+	Long:  ``,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+}
